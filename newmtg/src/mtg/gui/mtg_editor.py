fc423542889c8b53455b902337ea15412215e425
Daniel Barbeau <daniel.barbeau@sophia.inria.fr>
2011-01-13 16:01:14 +0000

    more work on mtg_editor. this is still quite dirty
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/vplants/vplants/trunk@10144 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- newmtg/src/mtg/gui/mtg_editor.py
+++ newmtg/src/mtg/gui/mtg_editor.py
@@ -18,11 +18,13 @@ __license__ = "Cecill-C"
 __revision__ = " $Id$ "
 
 from random import randint as rint
+from math import sin, cos, radians
 
 from openalea.grapheditor import qt, GraphAdapterBase
 from openalea.core.observer import Observed
 
 from openalea.mtg import MTG
+from openalea.mtg import algo
 
 ###############################################################
 ###############################################################
@@ -30,7 +32,8 @@ from openalea.mtg import MTG
 ###############################################################
 
 class ObservableMTG(GraphAdapterBase, Observed):
-    """An adapter to vplants.newmtg.mtg.MTG"""
+    """An adapter to vplants.newmtg.mtg.MTG. It has the role to
+    add notifications to actions performed on the underlying graph"""
     def __init__(self):
         GraphAdapterBase.__init__(self)
         Observed.__init__(self)
@@ -43,48 +46,59 @@ class ObservableMTG(GraphAdapterBase, Observed):
         return id_
 
     def add_vertex(self, vid, **kwargs):
+        vertex_added = [vid]
+        edge_added=[]
         pid = kwargs.pop("parent", None)
+        edge_type = kwargs.pop("edge_type", "<")
         g = self.graph
         if pid is None:
-            vid = g.add_component(g.root, child=vid, **kwargs)
+            vid = g.add_component(g.root, component_id=vid, **kwargs)
+            edge_added.append((g.root, vid))
         else:
-            edge_type = '+' if g.nb_children(pid) else '<'
-            vid = g.add_child(pid, child=vid,edge_type=edge_type, **kwargs)
+            if edge_type in ["<", "+"]:
+                vid = g.add_child(pid, child=vid,edge_type=edge_type, **kwargs)
+                edge_added.append((pid, vid))
+            elif edge_type == "/":
+                vid = g.add_component(pid, component_id=vid, **kwargs)
+                edge_added.append((pid, vid))
+            else: # add a complex
+                cpx_id = g.complex(pid)
+                if pid in list(g.component_roots(cpx_id)):
+                    g._id -= 1
+                    return
+                # TODO : What is the type of the edge?
+                g.add_child(cpx_id, child=vid, **kwargs)
+                g.add_component(vid, component_id=pid)
+                edge_added.append((cpx_id, vid))
+                edge_added.append((vid, pid))
+
+
         self.notify_listeners(("vertex_added", ("vertex",vid)))
-        if pid is not None:
-            print "woooo create an edge"
-            self.notify_listeners(("edge_added", ("default", (pid,vid), pid, vid)))
 
-    ######################
-    # -- NOT USED YET -- #
-    ######################
-    # def remove_vertex(self, vertex):
-    #     g = self.graph
-    #     edges = g.edges([vertex])
-    #     for src, tgt in edges:
-    #         self.remove_edge(src, tgt)
-    #     g.remove_node(vertex)
-    #     self.notify_listeners(("vertex_removed", ("vertex",vertex)))
+        for src, tgt in edge_added:
+            self.add_edge( src, tgt)
 
-    # def add_edge(self, src_vertex, tgt_vertex, **kwargs):
-    #     g = self.graph
-    #     edge = [src_vertex, tgt_vertex]
-    #     edge.sort(lambda x, y: cmp(id(x), id(y)))
-    #     edge = tuple(edge)
-    #     print "add", edge
-    #     if g.has_edge(*edge):
-    #         return
-    #     else:
-    #         g.add_edge(*edge, **kwargs)
-    #         self.notify_listeners(("edge_added", ("default", edge, src_vertex, tgt_vertex)))
+    def remove_vertex(self, vertex):
+        g = self.graph
+        pid = g.parent(vertex)
+        children = list(g.children(vertex))
+        self.remove_edge(pid,vertex)
+        for cid in children:
+            self.remove_edge(vertex,cid)
+            self.add_edge(pid,cid)
+        g.remove_vertex(vertex, reparent_child=True)
+        self.notify_listeners(("vertex_removed", ("vertex",vertex)))
 
-    # def remove_edge(self, src_vertex, tgt_vertex):
-    #     edge =  [src_vertex, tgt_vertex]
-    #     edge.sort(lambda x, y: cmp(id(x), id(y)))
-    #     edge = tuple(edge)
-    #     print "remove", edge
-    #     self.graph.remove_edge(edge[0], edge[1])
-    #     self.notify_listeners(("edge_removed", ("default",edge)))
+    def add_edge(self, src_vertex, tgt_vertex, **kwargs):
+        g = self.graph
+        edge = src_vertex, tgt_vertex
+        self.notify_listeners(("edge_added", ("default", edge, src_vertex, tgt_vertex)))
+        self.notify_listeners(("vertex_event", (src_vertex, "refresh")))
+        self.notify_listeners(("vertex_event", (tgt_vertex, "refresh")))
+
+    def remove_edge(self, src_vertex, tgt_vertex):
+        edge = src_vertex, tgt_vertex
+        self.notify_listeners(("edge_removed", ("default",edge)))
 
     # def remove_edges(self, edges):
     #     GraphAdapterBase.remove_edges(self, (e for e in edges))
@@ -103,6 +117,11 @@ from PyQt4 import QtGui, QtCore
 class Vertex( qt.DefaultGraphicalVertex ):
     max_scale = 6
 
+    def __init__(self, *args, **kwargs):
+        qt.DefaultGraphicalVertex.__init__(self, *args, **kwargs)
+        self.setFlag(QtGui.QGraphicsItem.ItemIsFocusable, True)
+        self._label = QtGui.QGraphicsSimpleTextItem(self)
+
     def _mtg(self):
         return self.graph().graph
     mtg = property(_mtg)
@@ -118,9 +137,13 @@ class Vertex( qt.DefaultGraphicalVertex ):
         color = QtGui.QColor.fromHsvF(s, 1,1)
         brush = QtGui.QBrush(color)
         self.setBrush(brush)
+        self._label.setText(str(self.vertex()))
+        self._label.setPos(self.boundingRect().center()-self._label.boundingRect().bottomRight()/2)
 
         # -- call to parent handles the position --
         qt.DefaultGraphicalVertex.initialise_from_model(self)
+        self.setFocus()
+        self.setSelected(True)
 
     def store_view_data(self, **kwargs):
         vid = self.vertex()
@@ -132,9 +155,11 @@ class Vertex( qt.DefaultGraphicalVertex ):
 
     # -- Extend this if you want to handle more notifications
     # -- but be sure to call the one from the superclass too
-    # def notify(self, sender, event):
-    #     """Handle notifications from the vertex model"""
-    #     qt.DefaultGraphicalVertex.notify(self, sender, event)
+    def notify(self, sender, event):
+        """Handle notifications from the vertex model"""
+        if event == "refresh":
+            self.notify_position_change()
+        qt.DefaultGraphicalVertex.notify(self, sender, event)
 
     def default_position(self):
         """If there is no position obtained by get_view_data("position"),
@@ -144,18 +169,39 @@ class Vertex( qt.DefaultGraphicalVertex ):
     ########################################################
     # The following methods are meant to modify the model! #
     ########################################################
-    def add_child(self, *args):
+    def add_child(self, *args, **kwargs):
         """ This methods modifies the mtg by adding a child to this vertex.
         The view will be updated through the graph's notifications.
         """
         gm = self.graph()
-        pos = self.get_view_data("position")
-        gm.new_vertex(parent=self.vertex(), position=[pos[0]+rint(-20,20),pos[1]-40.] )
+        x, y = self.get_view_data("position")
+        edge_type =  kwargs.get("edge_type", "<")
+        if edge_type in ["+","<"]:
+            y -= 40
+        elif edge_type in ["\\","/"]:
+            pass #y==y
+
+        if edge_type == "<":
+            n = len(algo.sons(self.mtg, self.vertex(), EdgeType="<"))
+            if n > 0: #there can only be one successor
+                return
+        elif edge_type == "+":
+            children = algo.sons(self.mtg, self.vertex(), EdgeType="+")
+            n = len(children)
+            n = n if n < 60/30 else n+1
+            angle = -60 + n*30
+            x += sin(radians(angle))*80
+        elif edge_type == "/":
+            x += 200
+        elif edge_type == "\\":
+            x -= 200
+
+        gm.new_vertex(parent=self.vertex(), position=[x,y], edge_type=edge_type )
 
         # -- this is required to correctly display the edges right from the start --
         # -- it's a bug in grapheditor --
         self.notify_position_change()
-        print "Vertex.add_child", pos
+        print "Vertex.add_child", x, y
 
     def add_component(self, *args):
         """ This methods modifies the mtg by adding a component to this vertex.
@@ -166,6 +212,33 @@ class Vertex( qt.DefaultGraphicalVertex ):
     ##########################
     # Some Qt Event handling #
     ##########################
+    def keyPressEvent(self, event):
+        k = event.key()
+        edge_type = "<"
+        to_add = True
+        if k == QtCore.Qt.Key_Less:
+            edge_type="<"
+            print "succesor"
+        elif k == QtCore.Qt.Key_Plus :
+            edge_type="+"
+            print "branch"
+        elif k == QtCore.Qt.Key_Slash :
+            edge_type="/"
+            print "component"
+        elif k == QtCore.Qt.Key_Backslash :
+            edge_type="\\"
+            print "complex"
+        elif k == QtCore.Qt.Key_Delete :
+            to_add=False
+        else:
+            return
+        event.accept()
+        if to_add:
+            self.add_child(edge_type=edge_type)
+        else:
+            self.graph().remove_vertex(self.vertex())
+
+
     def mouseDoubleClickEvent(self, event):
         self.add_child()
 
@@ -182,6 +255,8 @@ class MtgView( qt.View ):
         self.cutRequest.connect(self.on_cut_request)
         self.pasteRequest.connect(self.on_paste_request)
         self.deleteRequest.connect(self.on_delete_request)
+
+
 
     def on_copy_request(self, view, scene, a):
         """Implements the mtg copy operation.
@@ -220,6 +295,9 @@ class MtgView( qt.View ):
         You can delete whatever you want: vertices, edges: you handle it!
         If event evaluation should stop set a.accept=True.
         """
+        # scene = self.scene()
+        # selectedItem = scene.get_selected_items(
+        # self.scene().graph().remove_vertex(
         print "on_delete_request"
 
 
@@ -275,6 +353,7 @@ def initialise_graph_view_from_model(graphView, graphModel):
 
 
 
+
 # -- This creates a GraphicalMtg factory, a class that creates views for MTGS --
 GraphicalMtgFactory = qt.QtGraphStrategyMaker( graphView            = MtgView,
                                                vertexWidgetMap      = {"vertex":Vertex},
@@ -301,10 +380,10 @@ if __name__ == "__main__":
             self.__graph = ObservableMTG()
             self.__graphView = GraphicalMtgFactory.create_view(self.__graph, parent=self)
 
-            for p in range(1):
-                print "got here!"
-                self.__graph.new_vertex(position=[10.0, 10.0],
-                                        color=QtGui.QColor(rint(0,255),rint(0,255),rint(0,255)))
+            # for p in range(1):
+            #     print "got here!"
+            #     self.__graph.new_vertex(position=[10.0, 10.0],
+            #                             color=QtGui.QColor(rint(0,255),rint(0,255),rint(0,255)))
 
             self.setCentralWidget(self.__graphView)
 
