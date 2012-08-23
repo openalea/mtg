fa669cb6c0d1f6da95f872ead90d145b1c5d8798
Christophe Pradal <christophe.pradal@inria.fr>
2012-08-23 12:39:23 +0000

    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/vplants/vplants/trunk@12678 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ newmtg/src/mtg/plantframe/color.py
@@ -0,0 +1,23 @@
+import numpy as np
+from pylab import cm, colorbar
+from matplotlib.colors import Normalize, LogNorm
+
+def colormap(g, property_name, cmap='jet',lognorm=True):
+    """ Compute a color property based on a given property and a colormap.
+
+    """
+    prop = g.property(property_name)
+    keys = prop.keys()
+    values = np.array(prop.values())
+    #m, M = int(values.min()), int(values.max())
+    _cmap = cm.get_cmap(cmap)
+    norm = Normalize() if not lognorm else LogNorm() 
+    values = norm(values)
+    #my_colorbar(values, _cmap, norm)
+
+    colors = (_cmap(values)[:,0:3])*255
+    colors = np.array(colors,dtype=np.int).tolist()
+
+    g.properties()['color'] = dict(zip(keys,colors))
+    return g
+
