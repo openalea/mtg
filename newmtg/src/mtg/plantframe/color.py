0f4e589d02675c3764c113b3dc43e9e856d5dfe4
Christophe Pradal <christophe.pradal@inria.fr>
2012-08-23 16:22:03 +0000

    refactor with colors radius and length
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/vplants/vplants/trunk@12682 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- newmtg/src/mtg/plantframe/color.py
+++ newmtg/src/mtg/plantframe/color.py
@@ -1,5 +1,6 @@
 import numpy as np
 from pylab import cm, colorbar
+import matplotlib
 from matplotlib.colors import Normalize, LogNorm
 
 def colormap(g, property_name, cmap='jet',lognorm=True):
@@ -23,4 +24,23 @@ def colormap(g, property_name, cmap='jet',lognorm=True):
 
     g.properties()['color'] = dict(zip(keys,colors))
     return g
+
+def hex2color(s):
+     return tuple([int(n, 16) for n in (s[1:3], s[3:5], s[5:7])])
+def lut(g, property_name, colors=[], N=0):
+    """ Compute a color property based on a given property and a colormap.
+
+    """
+    prop = g.property(property_name)
+    keys = prop.keys()
+    values = np.array(prop.values())
+    #m, M = int(values.min()), int(values.max())
+
+    if not colors:
+        colors = [hex2color(c) for c in matplotlib.colors.cnames.values()]
+
+    
+    g.properties()['color'] = dict((keys[i], colors[values[i]]) for i in range(len(keys)))
+    return g
+
 
