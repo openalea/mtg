d05fca7408c44f4a56a661536dae29facf047bd2
pradal <christophe.pradal@cirad.fr>
2025-05-09 11:50:04 +0200

    Fix error in version

--- src/openalea/mtg/version.py
+++ src/openalea/mtg/version.py
@@ -2,4 +2,11 @@ import importlib.metadata
 
 __version__ =  importlib.metadata.version("openalea.mtg")
 
-major, minor, post = __version__.split(".")
+numbers = __version__.split(".")
+
+try:
+    major, minor, post = numbers[0], numbers[1], numbers[2]
+    patch = '.'.join(numbers[3:])
+except IndexError:
+    major, minor, post = '2', '4', '0'
+    patch = '0'
